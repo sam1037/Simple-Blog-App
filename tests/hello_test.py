df0dd84315688f0b6581151a752d399eaf8cd2f4
sam1037 <samngai2522@gmail.com>
2025-05-18 14:39:10 +0800

    test db github action good?

--- tests/hello_test.py
+++ tests/hello_test.py
@@ -1,3 +1,13 @@
+import src.database.db_wrapper as db_wrapper
+import pytest
+from src.app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
 def test_greeting():
     print('Hello, world!')
 
@@ -15,5 +25,20 @@ def test_dummy():
 
 def my_sum(lst: list[int]) -> int:
     return sum(lst)
+
+# test regsiter a new user
+def test_register(client):
+    TEST_USERNAME = "test_alpha_beta"
+
+    # act
+    response = client.post("/register", data = {
+        'username': TEST_USERNAME,
+        'password': 'pw'
+    })
+
+    # assert
+    assert response.status_code in (200, 400)
+    assert db_wrapper.get_user_by_username(TEST_USERNAME) is not None
+
 
 my_sum([1,2,3,5])
