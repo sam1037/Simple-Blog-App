d2fcfb4c3dfc63dadb10c0c2fee3eaf0e63c5fe9
sam1037 <samngai2522@gmail.com>
2025-06-10 15:28:04 +0800

    feat: index page now display a like icon

--- src/static/main.js
+++ src/static/main.js
@@ -10,6 +10,13 @@ async function fetchPosts() {
     posts.forEach(post => {
         const curDiv = document.createElement("div");
         curDiv.className = "bg-white p-4 rounded shadow-sm"
+        const likeCount = post.like_count !== undefined ? post.like_count : 0; 
+        const thumbsUpSVG = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16px" height="16px" class="inline-block mr-1 align-middle">
+              <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z"/>
+            </svg>
+        `;
+
         curDiv.innerHTML = `
             <div class="flex justify-between items-start mb-1">
                 <h3 class="text-xl font-bold">${post.title}</h3>
@@ -18,7 +25,13 @@ async function fetchPosts() {
                     <button id="deleteBtn" class="ml-2 px-2 py-1 text-xs bg-red-500 text-white rounded hover:bg-red-600 delete-button" data-post-id="${post.post_id}">Delete</button>
                 </div>
             </div>
-            <div class="text-sm text-gray-500 mb-2">By ${post.author} <span style="float: right;">${post.date_posted}</span></div>
+            <div class="text-sm text-gray-500 mb-1"> 
+                By ${post.author} 
+                <span style="float: right;">${post.date_posted}</span>
+            </div>
+            <div class="text-sm text-gray-500 mb-2" style="clear: both;"> 
+                <span style="float: right;">${thumbsUpSVG}${likeCount}</span>
+            </div>
             <p class="text-gray-700 whitespace-pre-wrap">${post.content}</p>
         `
 
