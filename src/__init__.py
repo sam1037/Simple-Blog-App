42b7a408430948b22b16cd14bc775c712a7bd002
sam1037 <samngai2522@gmail.com>
2025-06-10 11:19:31 +0800

    refactor: use blueprint, created auth and blog bp

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,44 @@
 # maybe i should move things from src.app to here, and rename src to simple-blog-app
+"""The main Flask app"""
+
+import logging
+import os
+import sys
+
+# import click
+from flask import Flask
+
+from src import auth, blog
+
+app = Flask(__name__)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+app.secret_key = SECRET_KEY
+
+my_logger = logging.getLogger("my_flask_logger")
+my_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("[%(levelname)s] - %(filename)s:%(lineno)d - %(message)s")
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+if os.environ.get("FLASK_ENV") and os.environ.get("FLASK_ENV") == "dev":
+    console_handler.setLevel(logging.DEBUG)
+else:
+    console_handler.setLevel(logging.INFO)
+my_logger.addHandler(console_handler)
+
+app.register_blueprint(auth.bp)
+app.register_blueprint(blog.bp)
+
+
+# # test cli
+# @click.command("hello")
+# def hello_cmd():
+#     """A testing cmd that will echo 'hello'"""
+#     click.echo("hello")
+
+
+# app.cli.add_command(hello_cmd)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ src/blog.py
@@ -0,0 +1,112 @@
+"""This file is the blueprint for blog related stuff, e.g. write post, edit post, read post"""
+
+import logging
+
+from flask import (
+    Blueprint,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from src.auth import login_required, login_required_api
+from src.database import db_wrapper
+
+# ??? prefix url ??? do what, should i use
+bp = Blueprint("blog", __name__)
+my_logger = logging.getLogger("my_flask_logger")
+
+
+# Blog index
+# TODO rename this, future will serve as public blog section
+@bp.route("/index")
+def index():
+    # render differently depending on viewing as guest or login user
+    if "username" not in session:
+        return render_template("index.html", username="Guest")
+    else:
+        return render_template("index.html", username=session["username"])
+
+
+# ??? this is actually an API route???
+@bp.route("/get_posts")
+def get_posts():
+    posts = db_wrapper.get_all_posts()
+    return jsonify(posts)
+
+
+# Create new post
+@bp.route("/write_post", methods=["GET", "POST"])
+@login_required
+def write_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        author = session["username"]
+        # insert post to db
+        db_wrapper.insert_new_post(author, title, content)
+        return redirect(url_for("blog.index"))
+    return render_template("write_post.html")
+
+
+# Edit an exisiting post
+@bp.route("/edit_post/<post_id>", methods=["GET", "POST"])
+@login_required
+def edit_post(post_id):
+    # get the post by post id first and check if valid post id
+    post = db_wrapper.get_post_by_id(post_id)
+    my_logger.debug(f"editing this post: {post}")
+    my_logger.debug(f"method: {request.method}")
+    if not post:
+        return jsonify(
+            {"message": "Some error occured when trying to retrieve the post"}
+        ), 403
+
+    # check if user if authorized
+    if session.get("username") != post["author"]:
+        return jsonify({"message": "You are not authorized to edit this post"}), 403
+
+    # GET route: verify, redirect to the write post page with some fields filled
+    if request.method == "GET":
+        # ??? should I use redirect or render template in this line???
+        return render_template(
+            "write_post.html", title=post["title"], text_content=post["content"]
+        )
+
+    # POST route: verify, save edit to the db
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        db_wrapper.edit_post_by_id(post_id, title=title, content=content)
+        return redirect(
+            url_for("blog.index")
+        )  # ??? redirect or render template here ???
+
+    return render_template("index.html")  # ??? redirect or render template here ???
+
+
+# Delete a post by post id
+# ??? is this like an api route?
+# TODO should i rename this to sth like 'api/posts, following REST?'
+@bp.route("/delete_post/<post_id>", methods=["DELETE"])
+@login_required_api
+def delete_post(post_id):
+    # Verify if is author deleting his post
+    username = session.get("username")
+    if not username:
+        return jsonify({"message": "Unauthorized"}), 401
+
+    post = db_wrapper.get_post_by_id(post_id)
+    if not post:
+        return jsonify({"message": "Post not found"}), 404
+
+    if post["author"] != username:
+        return jsonify({"message": "You are not authorized to delete this post"}), 403
+
+    # Delete it
+    if db_wrapper.delete_post_by_id(post_id):
+        return jsonify({"message": "Post deleted successfully."})
+    return jsonify({"message": "Error occured during post deletion"}), 404
